2fce7c32e01e09e598407795c2a105ac970a1190
zoltan.kasza <zoltan.kasza@ibello.hu>
2020-08-25 13:24:53 +0200

    fix error, add methods to fill out the form to send a message

--- ibello-hu/src/main/java/hu/ibello/demo/SendingMessageWorkflow.java
+++ ibello-hu/src/main/java/hu/ibello/demo/SendingMessageWorkflow.java
@@ -1,31 +1,43 @@
 package hu.ibello.demo;
 
+import hu.ibello.demo.steps.NavigationSteps;
+import hu.ibello.demo.steps.SendingMessageSteps;
 import hu.ibello.steps.StepLibrary;
 
 //@Name("Sending message")
 public class SendingMessageWorkflow extends StepLibrary{
 
-    public void i_see_that_the_there_is_an_error_message() {
-        // TODO auto-generated method
-    }
-
-    public void i_send_the_message() {
-        // TODO auto-generated method
-    }
+    private SendingMessageSteps sendingMessageSteps;
+    private NavigationSteps navigationSteps;
 
     public void i_want_to_send_a_new_message() {
-        // TODO auto-generated method
-    }
-
-    public void the_message_will_be_invalid() {
-        // TODO auto-generated method
+        navigationSteps.expect_homepage_is_loaded();
     }
 
     public void the_name_will_be_invalid() {
         // TODO auto-generated method
+        sendingMessageSteps.i_use_$_invalid_name();
+    }
+
+    public void the_email_will_be_invalid() {
+        sendingMessageSteps.i_use_$_invalid_email();
     }
 
     public void the_subject_will_be_invalid() {
+        // TODO auto-generated method
+        sendingMessageSteps.i_use_$_invalid_subject();
+    }
+
+    public void the_message_will_be_invalid() {
+        // TODO auto-generated method
+        sendingMessageSteps.i_use_$_invalid_message();
+    }
+
+    public void i_send_the_message() {
+        sendingMessageSteps.i_send_the_message();
+    }
+
+    public void i_see_that_the_there_is_an_error_message() {
         // TODO auto-generated method
     }
 
--- ibello-hu/src/main/java/hu/ibello/demo/steps/SendingMessageSteps.java
+++ ibello-hu/src/main/java/hu/ibello/demo/steps/SendingMessageSteps.java
@@ -1,10 +1,27 @@
 package hu.ibello.demo.steps;
 
 import hu.ibello.core.Name;
+import hu.ibello.demo.pages.HomePage;
 import hu.ibello.steps.StepLibrary;
-
 
 @Name("Sending message steps")
 public class SendingMessageSteps extends StepLibrary {
 
+    private HomePage homePage;
+
+    public void i_send_the_message() {
+        homePage.click_send_message_button();
+    }
+
+    public void i_use_$_invalid_name() {
+    }
+
+    public void i_use_$_invalid_email() {
+    }
+
+    public void i_use_$_invalid_subject() {
+    }
+
+    public void i_use_$_invalid_message() {
+    }
 }
