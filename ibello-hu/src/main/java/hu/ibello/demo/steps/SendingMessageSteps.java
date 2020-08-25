8f47085045dd8b5cc73d3f7b4d793276322d31b0
zoltan.kasza <zoltan.kasza@ibello.hu>
2020-08-25 10:51:56 +0200

    add sendingMEssageSteps

---
+++ ibello-hu/src/main/java/hu/ibello/demo/steps/SendingMessageSteps.java
@@ -0,0 +1,10 @@
+package hu.ibello.demo.steps;
+
+import hu.ibello.core.Name;
+import hu.ibello.steps.StepLibrary;
+
+
+@Name("Sending message steps")
+public class SendingMessageSteps extends StepLibrary {
+
+}
