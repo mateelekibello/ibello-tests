16a15f937a3aceb1c43a4ae38ac9e5114bbda1ec
zoltan.kasza <zoltan.kasza@ibello.hu>
2020-08-26 10:10:28 +0200

    add: feature and workflow for ordering with existing user

---
+++ ibello-hu/src/main/java/hu/ibello/demo/OrderingWithExistingUserWorkflow.java
@@ -0,0 +1,43 @@
+package hu.ibello.demo;
+
+import hu.ibello.demo.model.ContactData;
+import hu.ibello.demo.steps.NavigationSteps;
+import hu.ibello.demo.steps.SendingMessageSteps;
+import hu.ibello.steps.StepLibrary;
+
+//@Name("Ordering with existing user")
+public class OrderingWithExistingUserWorkflow extends StepLibrary{
+
+    public void i_navigate_to_the_order_page() {
+        // TODO auto-generated method
+    }
+
+    public void the_username_will_be_invalid() {
+        // TODO auto-generated method
+    }
+
+    public void the_password_will_be_invalid() {
+        // TODO auto-generated method
+    }
+
+    public void the_product_will_be_invalid() {
+        // TODO auto-generated method
+    }
+
+    public void i_send_the_order() {
+        // TODO auto-generated method
+    }
+
+    public void i_see_that_there_is_an_error_message() {
+        // TODO auto-generated method
+    }
+
+    public void i_use_valid_username_and_password() {
+        // TODO auto-generated method
+    }
+
+    public void i_see_that_the_order_is_successfull() {
+        // TODO auto-generated method
+    }
+
+}
