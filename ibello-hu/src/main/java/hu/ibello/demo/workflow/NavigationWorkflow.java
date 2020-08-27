160cc17ae1f192833b65df7a03c282f790e428f7
zoltan.kasza <zoltan.kasza@ibello.hu>
2020-08-27 12:50:12 +0200

    add: navigation workflow

---
+++ ibello-hu/src/main/java/hu/ibello/demo/workflow/NavigationWorkflow.java
@@ -0,0 +1,52 @@
+package hu.ibello.demo.workflow;
+
+import hu.ibello.core.Name;
+import hu.ibello.steps.StepLibrary;
+
+@Name("navigation")
+public class NavigationWorkflow extends StepLibrary{
+
+    public void i_am_on_about_page() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_homepage() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_product_page() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_services_page() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_support_page() {
+        // TODO auto-generated method
+    }
+
+    public void i_navigate_back_to_homepage() {
+        // TODO auto-generated method
+    }
+
+    public void i_navigate_to_about_page() {
+        // TODO auto-generated method
+    }
+
+    public void i_navigate_to_product_page() {
+        // TODO auto-generated method
+    }
+
+    public void i_navigate_to_services_page() {
+        // TODO auto-generated method
+    }
+
+    public void i_navigate_to_support_page() {
+        // TODO auto-generated method
+    }
+
+    public void test_init() {
+        // TODO auto-generated method
+    }
+}
