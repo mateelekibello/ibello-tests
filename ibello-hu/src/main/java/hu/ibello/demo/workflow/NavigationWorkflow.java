c0eb192c208898f486b4b14f00102c52518c5115
zoltan.kasza <zoltan.kasza@ibello.hu>
2020-08-31 17:46:26 +0200

    add: generated methods to NavigationWorkflow

--- ibello-hu/src/main/java/hu/ibello/demo/workflow/NavigationWorkflow.java
+++ ibello-hu/src/main/java/hu/ibello/demo/workflow/NavigationWorkflow.java
@@ -9,47 +9,207 @@ public class NavigationWorkflow extends StepLibrary{
 
     private NavigationSteps navigationSteps;
 
-    public void i_am_on_about_page() {
-        navigationSteps.i_am_on_about_page();
-    }
-
-    public void i_am_on_homepage() {
-        navigationSteps.i_open_the_homepage();
-    }
-
-    public void i_am_on_product_page() {
-        navigationSteps.i_am_on_product_page();
-    }
-
-    public void i_am_on_services_page() {
-        navigationSteps.i_am_on_services_page();
-    }
-
-    public void i_am_on_support_page() {
+    public void from_documentations_page_i_navigate_to_change_log_page() {
         // TODO auto-generated method
     }
 
-    public void i_navigate_back_to_homepage() {
-        navigationSteps.i_navigate_back_to_homepage();
+    public void from_homepage_header_i_navigate_to_product_page() {
+        // TODO auto-generated method
     }
 
-    public void i_navigate_to_about_page() {
-        navigationSteps.i_navigate_from_homepage_to_about_page();
+    public void from_homepage_header_i_navigate_to_services_page() {
+        // TODO auto-generated method
     }
 
-    public void i_navigate_to_product_page() {
-        navigationSteps.i_navigate_from_homepage_to_product_page();
+    public void from_homepage_i_navigate_to_about_page() {
+        // TODO auto-generated method
     }
 
-    public void i_navigate_to_services_page() {
-        navigationSteps.i_navigate_from_homepage_to_services_page();
+    public void from_homepage_i_navigate_to_documentations_page() {
+        // TODO auto-generated method
     }
 
-    public void i_navigate_to_support_page() {
-        navigationSteps.i_navigate_from_homepage_to_support_page();
+    public void from_homepage_i_navigate_to_interest_page() {
+        // TODO auto-generated method
     }
 
-    public void test_init() {
+    public void from_homepage_i_navigate_to_pricing_page() {
+        // TODO auto-generated method
+    }
+
+    public void from_homepage_i_navigate_to_product_page() {
+        // TODO auto-generated method
+    }
+
+    public void from_homepage_i_navigate_to_services_page() {
+        // TODO auto-generated method
+    }
+
+    public void from_homepage_i_navigate_to_support_page() {
+        // TODO auto-generated method
+    }
+
+    public void from_installation_documentation_page_i_navigate_to_documentations_page() {
+        // TODO auto-generated method
+    }
+
+    public void from_interest_page_i_navigate_to_documentations_page() {
+        // TODO auto-generated method
+    }
+
+    public void from_interest_page_i_navigate_to_installation_documentation_page() {
+        // TODO auto-generated method
+    }
+
+    public void from_product_page_header_i_navigate_to_product_page() {
+        // TODO auto-generated method
+    }
+
+    public void from_product_page_i_navigate_to_order_page() {
+        // TODO auto-generated method
+    }
+
+    public void from_product_page_i_order_a_hunter_product() {
+        // TODO auto-generated method
+    }
+
+    public void from_product_page_i_order_a_master_hunter_product() {
+        // TODO auto-generated method
+    }
+
+    public void from_product_page_i_order_a_sentinel_product() {
+        // TODO auto-generated method
+    }
+
+    public void from_product_page_i_order_an_outpost_product() {
+        // TODO auto-generated method
+    }
+
+    public void from_services_page_i_request_guotation() {
+        // TODO auto-generated method
+    }
+
+    public void from_support_page_header_i_navigate_to_services_page() {
+        // TODO auto-generated method
+    }
+
+    public void from_support_page_i_navigate_to_change_log_page() {
+        // TODO auto-generated method
+    }
+
+    public void from_support_page_i_navigate_to_documentations_page() {
+        // TODO auto-generated method
+    }
+
+    public void from_support_page_i_navigate_to_installation_documentation_page() {
+        // TODO auto-generated method
+    }
+
+    public void from_support_page_i_navigate_to_product_page() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_change_log_page() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_documentations_page() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_installation_documentation_page() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_interest_page() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_order_page_and_hunter_is_selected() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_order_page_and_master_hunter_is_selected() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_order_page_and_no_product_is_selected() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_order_page_and_outpost_is_selected() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_order_page_and_sentinel_is_selected() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_pricing_page() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_quotation_page_and_automated_tests_is_selected() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_quotation_page_and_consultancy_is_selected() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_quotation_page_and_dev_and_test_training_is_selected() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_quotation_page_and_online_automated_test_is_selected() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_quotation_page_and_test_framework_is_selected() {
+        // TODO auto-generated method
+    }
+
+    public void i_am_on_quotation_page_without_selected_service() {
+        // TODO auto-generated method
+    }
+
+    public void i_navigate_directly_to_about_page_using_the_navbar() {
+        // TODO auto-generated method
+    }
+
+    public void i_navigate_directly_to_homepage_using_the_navbar() {
+        // TODO auto-generated method
+    }
+
+    public void i_navigate_directly_to_product_page_using_the_navbar() {
+        // TODO auto-generated method
+    }
+
+    public void i_navigate_directly_to_services_page_using_the_navbar() {
+        // TODO auto-generated method
+    }
+
+    public void i_navigate_directly_to_support_page_using_the_navbar() {
+        // TODO auto-generated method
+    }
+
+    public void i_request_quotation_for_automated_tests() {
+        // TODO auto-generated method
+    }
+
+    public void i_request_quotation_for_consultancy() {
+        // TODO auto-generated method
+    }
+
+    public void i_request_quotation_for_dev_and_test_training() {
+        // TODO auto-generated method
+    }
+
+    public void i_request_quotation_for_online_automated_test() {
+        // TODO auto-generated method
+    }
+
+    public void i_request_quotation_for_test_framework() {
         // TODO auto-generated method
     }
 }
